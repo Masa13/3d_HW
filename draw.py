589756e3294270c5e6d58c73a7f61020c37e739a
Masa13 <masahero55@gmail.com>
2016-03-29 05:01:56 -0400

    qwq

--- draw.py
+++ draw.py
@@ -6,15 +6,12 @@ import math
 def add_box( points, x, y, z, width, height, depth ):
     add_edge(points, x, y, z, x, y, z)
     add_edge(points, x, y+height, z, x, y+height, z)
-
     add_edge(points, x, y, z+depth, x, y, z+depth)
     add_edge(points, x, y+height, z+depth, x, y+height, z+depth)
-
-    add_edge(points, x+width, y, z, x+width, y, z)
-    add_edge(points, x+width, y+height, z, x+width, y+height, z)
-
-    add_edge(points, x+width, y, z+depth, x+width, y, z+depth)
-    add_edge(points, x+width, y+height, z+depth, x+width, y+height, z+depth)
+    add_edge(points, x + width, y, z, x + width, y, z)
+    add_edge(points, x + width, y+height, z, x+width, y+height, z)
+    add_edge(points, x + width, y, z+depth, x+width, y, z+depth)
+    add_edge(points, x + width, y+height, z+depth, x+width, y+height, z+depth)
 
 def add_sphere( points, cx, cy, cz, r, step ):
     sphere = []
